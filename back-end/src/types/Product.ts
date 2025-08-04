eba32b4d90a16d5ae813c76142477b449222314f
AlaeMoh <alae.muhammad@gmail.com>
2025-08-04 19:52:37 +0300

    Initial commit - Frontend & Backend

---
+++ back-end/src/types/Product.ts
@@ -0,0 +1,13 @@
+export type Products={
+      productId: Number,
+      productSku: string,
+      productName:string ,
+      productPrice: number,
+      productShortName: string,
+      productDescription: string,
+      createdDate:string,
+      deliveryTimeSpan: string,
+      categoryId: number,
+      productImageUrl: string,
+      categoryName: string
+}
